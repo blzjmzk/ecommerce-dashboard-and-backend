7cc9e0897ff2ac814426aa2ac3a1084a1bc69fca
Błażej Mzyk <blazej.mzyk@gmail.com>
2024-02-22 14:06:08 +0100

    Add settings form - preparations

---
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
+
+interface SettingsFormProps {
+  initialData: Store;
+}
+
+const SettingsForm = ({ initialData }: SettingsFormProps) => {
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading title="Settings" description="Manage store preferences" />
+        <Button variant="destructive" size="icon" onClick={() => {}}>
+          <Trash className="h-4 w-4" />
+        </Button>
+      </div>
+      <Separator />
+    </>
+  );
+};
+
+export default SettingsForm;
