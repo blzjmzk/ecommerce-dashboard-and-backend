ff68d9445abd3db550ef8071967d91c0c7cbbc6f
Błażej Mzyk <blazej.mzyk@gmail.com>
2024-02-23 16:41:10 +0100

    Add alert modal

--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -6,7 +6,7 @@ import { StoreModal } from "@/components/modals/store-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
-  //unikamy hydration errors -> chcemy renderować modal w layout, który jest SSR, upewniamy się, ze do czasu jak zachodzi SSR (jak aplikacja nie jest zamontowana) zwracamy null
+  //unikamy hydration errors -> chcemy renderować modal w layout, który (layout) jest SSR, upewniamy się, ze do czasu jak zachodzi SSR (jak aplikacja nie jest zamontowana) zwracamy null
   useEffect(() => {
     setIsMounted(true);
   }, []);
